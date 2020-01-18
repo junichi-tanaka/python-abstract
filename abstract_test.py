96d37b00ad13f3439264c87057f9b3f9a6eae913
Junichi TANAKA <junsan45@gmail.com>
2020-01-19 01:34:03 +0900

    Add test with raise exception (it fails with python 3.x)

--- abstract_test.py
+++ abstract_test.py
@@ -14,5 +14,13 @@ class TestAbstract(unittest.TestCase):
         actual = human.say()
         self.assertEqual(expected, actual)
 
+    # https://stackoverflow.com/questions/35673474/using-abc-abcmeta-in-a-way-it-is-compatible-both-with-python-2-7-and-python-3-5
+    def test_raises_exception(self):
+        with self.assertRaises(TypeError) as error:
+            processor = Abstract()
+        actual = str(error.exception)
+        expected = "Can't instantiate abstract class Abstract with abstract methods say"
+        self.assertEqual(expected, actual)
+
 if __name__ == "__main__":
     unittest.main()
